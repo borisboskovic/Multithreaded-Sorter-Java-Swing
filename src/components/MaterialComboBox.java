494efad46b73b199009488d134550fc87809e525
Boris Boskovic <boris.boskovic92@gmail.com>
2020-03-29 23:45:16 +0200

    Multithreaded test in progress

--- src/components/MaterialComboBox.java
+++ src/components/MaterialComboBox.java
@@ -19,100 +19,97 @@ import settings.ColorTheme;
 import settings.Themes;
 
 @SuppressWarnings("serial")
-public class MaterialComboBox<E> extends JComboBox<E>{
+public class MaterialComboBox<E> extends JComboBox<E> {
 
 	public MaterialComboBox(Vector<E> items) {
 		super(items);
-		ColorTheme th= Themes.getCurrentTheme();
+		ColorTheme th = Themes.getCurrentTheme();
 		Font fnt = th.getFonts().getMainButtonFont();
 		Font large = new Font(fnt.getName(), fnt.getStyle(), 32);
 		setFont(large);
 		setBackground(th.getThemeColor());
 		setForeground(th.getTextSecondaryColor());
-		
+
 		setEditor(new CustomComboBoxEditor());
 		setEditable(true);
 		setRenderer(new CustomCellRenderer(getRenderer()));
-		
+
 		setUI(new BasicComboBoxUI() {
 			@Override
 			protected JButton createArrowButton() {
 				JButton b = super.createArrowButton();
-				b=new JButton("...");
+				b = new JButton("\u02c5");
 				b.setFont(large);
-				b.setBackground(th.getThemeColor());
 				b.setBackground(th.getAccentColor());
 				b.setForeground(th.getTextSecondaryColor());
-		        b.setBorderPainted(false);
-		        return b; 
+				b.setBorderPainted(false);
+				return b;
 			}
 		});
-		
+
 	}
-	
+
 }
 
 class CustomCellRenderer extends DefaultListCellRenderer {
 
-	  private ListCellRenderer defaultRenderer;
+	private ListCellRenderer defaultRenderer;
 
-	  public CustomCellRenderer(ListCellRenderer defaultRenderer) {
-	    this.defaultRenderer = defaultRenderer;
-	  }
-
-	  @Override
-	  public Component getListCellRendererComponent(JList list, Object value,
-	      int index, boolean isSelected, boolean cellHasFocus) {
-	    Component c = defaultRenderer.getListCellRendererComponent(list, value,
-	        index, isSelected, cellHasFocus);
-	    ColorTheme th=Themes.getCurrentTheme();
-	    if (c instanceof JLabel) {
-	      if (isSelected) {
-	        c.setBackground(th.getAccentColor());
-	        c.setForeground(th.getTextSecondaryColor());
-	        ((JLabel) c).setBorder(null);
-	      } else {
-	      }
-	    } else {
-	      c = super.getListCellRendererComponent(list, value, index, isSelected,
-	          cellHasFocus);
-	    }
-	    return c;
-	  }
+	public CustomCellRenderer(ListCellRenderer defaultRenderer) {
+		this.defaultRenderer = defaultRenderer;
 	}
 
+	@Override
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
+			boolean cellHasFocus) {
+		Component c = defaultRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+		ColorTheme th = Themes.getCurrentTheme();
+		if (c instanceof JLabel) {
+			if (isSelected) {
+				c.setBackground(th.getAccentColor());
+				c.setForeground(th.getTextSecondaryColor());
+				((JLabel) c).setBorder(null);
+			} else {
+			}
+		} else {
+			c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+		}
+		return c;
+	}
+}
+
 class CustomComboBoxEditor extends BasicComboBoxEditor {
-    private JLabel label = new JLabel();
-    private JPanel panel = new JPanel();
-    private Object selectedItem;
-     
-    public CustomComboBoxEditor() {
-    	    	
-    	ColorTheme th=Themes.getCurrentTheme();
-    	Font ft=th.getFonts().getMainButtonFont();
-    	Font large = new Font(ft.getName(), ft.getStyle(), 32);
-    	
-    	label.setOpaque(false);
-    	label.setFont(large);
-    	label.setForeground(th.getTextSecondaryColor());
-    	
+	private JLabel label = new JLabel();
+	private JPanel panel = new JPanel();
+	private Object selectedItem;
+
+	public CustomComboBoxEditor() {
+
+		ColorTheme th = Themes.getCurrentTheme();
+		Font ft = th.getFonts().getMainButtonFont();
+		Font large = new Font(ft.getName(), ft.getStyle(), 32);
+
+		label.setOpaque(false);
+		label.setFont(large);
+		label.setForeground(th.getTextSecondaryColor());
+
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        panel.add(label);
-        panel.setBackground(th.getThemeColor());
-        panel.setBorder(null);
-    }
-     
-    public Component getEditorComponent() {
-        return this.panel;
-    }
-     
-    public Object getItem() {
-        return this.selectedItem.toString();
-    }
-     
-    public void setItem(Object item) {
-        this.selectedItem = item;
-        label.setText(item.toString());
-    }
-     
+		panel.add(label);
+		panel.setBackground(th.getThemeColor());
+		panel.setBorder(null);
+	}
+
+	public Component getEditorComponent() {
+		return this.panel;
+	}
+
+	public Object getItem() {
+		return this.selectedItem.toString();
+	}
+
+	public void setItem(Object item) {
+		this.selectedItem = item;
+		label.setText(item.toString());
+	}
+
 }
--- src/view/MultithreadedTestView.java
+++ src/view/MultithreadedTestView.java
@@ -1,14 +1,206 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.RenderingHints;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ComboBoxEditor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicComboBoxUI;
 
+import components.MaterialButton;
+import components.MaterialComboBox;
+import components.MaterialTextField;
 import model.MultithreadedTestModel;
+import settings.ColorTheme;
+import settings.Themes;
 
-public class MultithreadedTestView extends JPanel{
+@SuppressWarnings("serial")
+public class MultithreadedTestView extends JPanel {
 	private MultithreadedTestModel model;
+
+	private JTextField path;
+	private JButton browseBtn;
+	private JComboBox<Integer> maxThreads;
+	private JTextField minFilesPerThread;
+	private JButton sortBtn;
 
 	public MultithreadedTestView(MultithreadedTestModel model) {
 		this.model = model;
+
+		setLayout(new GridLayout(1, 1));
+
+		JPanel mainPanel = new JPanel();
+		BoxLayout mainPanelLayout = new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS);
+		mainPanel.setLayout(mainPanelLayout);
+		mainPanel.setOpaque(false);
+		mainPanel.setMaximumSize(new Dimension(2000, 500));
+
+		ColorTheme th = Themes.getCurrentTheme();
+		mainPanel.add(createPathPanel(th));
+		mainPanel.add(createAmmountPanel(th));
+		mainPanel.add(createNoticePanel(th));
+		mainPanel.add(createBtnPanel(th));
+
+		add(mainPanel);
+
+		setOpaque(false);
+	}
+
+	private JPanel createPathPanel(ColorTheme th) {
+		Font lblFont = th.getFonts().getLabelFont();
+
+		JPanel panel = new JPanel();
+		BoxLayout ly = new BoxLayout(panel, BoxLayout.LINE_AXIS);
+		panel.setLayout(ly);
+		panel.setMaximumSize(new Dimension(2000, 46));
+		panel.setOpaque(false);
+		JLabel label = new JLabel("Path: "); // TODO: Lokalizacija
+		label.setFont(lblFont);
+		this.path = new MaterialTextField(20);
+		this.browseBtn = new MaterialButton("...");
+
+		JPanel container = new JPanel();
+		container.setBorder(BorderFactory.createEmptyBorder(50, 100, 25, 100));
+		container.setLayout(new GridLayout(1, 1));
+		container.setMaximumSize(new Dimension(2000, 118));
+		container.setOpaque(false);
+
+		panel.add(label);
+		panel.add(path);
+		panel.add(Box.createHorizontalStrut(20));
+		panel.add(browseBtn);
+		container.add(panel);
+		return container;
+	}
+
+	private JPanel createAmmountPanel(ColorTheme th) {
+		JPanel panel = new JPanel();
+		Font lblFont = th.getFonts().getLabelFont();
+
+		BoxLayout ly = new BoxLayout(panel, BoxLayout.LINE_AXIS);
+		panel.setLayout(ly);
+		panel.setMaximumSize(new Dimension(2000, 46));
+		panel.setOpaque(false);
+
+		JLabel lbl1 = new JLabel("Max threads: ");
+		lbl1.setFont(lblFont);
+
+		JLabel lbl2 = new JLabel("Min files per thread: ");
+		lbl2.setFont(lblFont);
+
+		// Thread Count combo box
+		Vector<Integer> count = new Vector<>();
+		count.add(1);
+		count.add(2);
+		count.add(4);
+		count.add(8);
+		count.add(16);
+		count.add(32);
+		this.maxThreads = new MaterialComboBox<>(count);
+		this.maxThreads.setFont(lblFont);
+		this.maxThreads.setPreferredSize(new Dimension(90, 46));
+		this.maxThreads.setMaximumSize(new Dimension(100, 100));
+		this.maxThreads.setUI(new BasicComboBoxUI() {
+			@Override
+			protected JButton createArrowButton() {
+				JButton b = super.createArrowButton();
+				b = new JButton("\u02c5");
+				b.setFont(lblFont);
+				b.setBackground(th.getAccentColor());
+				b.setForeground(th.getTextSecondaryColor());
+				b.setBorderPainted(false);
+				return b;
+
+			}
+		});
+		ComboBoxEditor editor = this.maxThreads.getEditor();
+		JPanel editorPanel = (JPanel) editor.getEditorComponent();
+		editorPanel.setFont(lblFont);
+		JLabel label = (JLabel) editorPanel.getComponent(0);
+		label.setFont(lblFont);
+		label.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+		editorPanel.setLayout(new GridLayout(1, 1));
+		this.maxThreads.setEditor(editor);
+
+		this.minFilesPerThread = new MaterialTextField(3);
+		this.minFilesPerThread.setFont(lblFont);
+		this.minFilesPerThread.setMaximumSize(new Dimension(100, 100));
+
+		JPanel container = new JPanel();
+		container.setBorder(BorderFactory.createEmptyBorder(20, 100, 25, 100));
+		container.setMaximumSize(new Dimension(2000, 91));
+		container.setLayout(new GridLayout(1, 1));
+		container.setOpaque(false);
+
+		panel.add(Box.createHorizontalGlue());
+		panel.add(lbl1);
+		panel.add(maxThreads);
+		panel.add(Box.createHorizontalGlue());
+		panel.add(lbl2);
+		panel.add(minFilesPerThread);
+		panel.add(Box.createHorizontalGlue());
+		container.add(panel);
+
+		return container;
+	}
+
+	private JPanel createNoticePanel(ColorTheme th) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		panel.setLayout(new GridLayout(1, 1));
+		panel.setMaximumSize(new Dimension(2000, 150));
+		panel.setOpaque(false);
+
+		JLabel label = new JLabel(
+				"<html>* Ovaj tekst treba zamijeniti stvarnim tekstom. Ovaj tekst treba zamijeniti stvarnim tekstom.Ovaj tekst treba zamijeniti stvarnim tekstom.Ovaj tekst treba zamijeniti stvarnim tekstom.Ovaj tekst treba zamijeniti stvarnim tekstom.</html>");
+
+		label.setFont(th.getFonts().getNoteFont());
+		label.setForeground(th.getAccentColor());
+		panel.add(label);
+
+		JPanel container = new JPanel(new GridLayout(1, 1));
+		container.setMaximumSize(new Dimension(2000, 175));
+		container.setBorder(BorderFactory.createEmptyBorder(0, 50, 25, 50));
+		container.setOpaque(false);
+		container.add(panel);
+		return container;
+	}
+
+	private JPanel createBtnPanel(ColorTheme th) {
+		Font fontLg = new Font(th.getFonts().getMainButtonFont().getName(), Font.TRUETYPE_FONT, 32);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel.setMaximumSize(new Dimension(2000, 80));
+		panel.setOpaque(false);
+		this.sortBtn = new MaterialButton("   Sort   "); // TODO: Lokalizacija
+		this.sortBtn.setFont(fontLg);
+
+		panel.add(sortBtn);
+		return panel;
 	}
 	
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D graphics2d = (Graphics2D)g;
+		graphics2d.setColor(Themes.getCurrentTheme().getSectionColor());
+		graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics2d.fillRoundRect(0, 0, getSize().width, getSize().height, 50, 50);
+		super.paint(g);
+	}
+
 }
